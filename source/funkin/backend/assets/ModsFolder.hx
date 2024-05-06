f29eb3610c4312acb18e208f3304df660cf2ff30
NewMidnightXD <160505130+NewMidnightXD@users.noreply.github.com>
2024-05-06 12:41:00 -0300

    Update ModsFolder.hx

--- source/funkin/backend/assets/ModsFolder.hx
+++ source/funkin/backend/assets/ModsFolder.hx
@@ -39,7 +39,7 @@ class ModsFolder {
 	/**
 	 * Path to the `addons` folder.
 	 */
-	public static var addonsPath:String = SUtil.getStorageDirectory(true) + "./addons/";
+	public static var addonsPath:String = "./addons/";
 
 	/**
 	 * If accessing a file as assets/data/global/LIB_mymod.hx should redirect to mymod:assets/data/global.hx
