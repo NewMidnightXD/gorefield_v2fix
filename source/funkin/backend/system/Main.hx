4dee6e69ac3f8209b2016f4cbc35549120e68e08
NewMidnightXD <160505130+NewMidnightXD@users.noreply.github.com>
2024-05-05 22:15:18 -0300

    Update Main.hx

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -34,6 +34,7 @@ import sys.thread.Thread;
 import sys.io.File;
 #end
 import funkin.backend.assets.ModsFolder;
+import lime.system.System as LimeSystem;
 
 class Main extends Sprite
 {
@@ -74,7 +75,16 @@ class Main extends Sprite
 
 		instance = this;
 
+		#if mobile
+		#if android
+		SUtil.doPermissionsShit();
+		#end
+		Sys.setCwd(SUtil.getStorageDirectory(false));
+		#end
+
 		CrashHandler.init();
+
+		#if !html5 framerateSprite = new funkin.backend.system.framerate.Framerate(); #end
 
 		doPermissionsShit();
 		
@@ -89,8 +99,13 @@ class Main extends Sprite
 		
 		addChild(game = new FunkinGame(gameWidth, gameHeight, MainState, Options.framerate, Options.framerate, skipSplash, startFullscreen));
 
-		addChild(framerateSprite = new funkin.backend.system.framerate.Framerate());
+		#if android FlxG.android.preventDefaultKeys = [BACK]; #end
+
+		#if !html5
+		addChild(framerateSprite);
+		FlxG.stage.window.onResize.add((w:Int, h:Int) -> framerateSprite.setScale());
 		SystemInfo.init();
+		#end
 
 		   
 	Lib.application.onExit.add(function(i:Int) {
@@ -157,12 +172,12 @@ class Main extends Sprite
 		#if (sys && TEST_BUILD)
 			trace("Used cne test / cne build. Switching into source assets.");
 			#if MOD_SUPPORT
-				ModsFolder.modsPath = './${pathBack}mods/';
-				ModsFolder.addonsPath = './${pathBack}addons/';
+				ModsFolder.modsPath = Sys.getCwd() + '${pathBack}mods/';
+				ModsFolder.addonsPath = Sys.getCwd() + '${pathBack}addons/';
 			#end
-			Paths.assetsTree.__defaultLibraries.push(ModsFolder.loadLibraryFromFolder('assets', './${pathBack}assets/', true));
+			Paths.assetsTree.__defaultLibraries.push(ModsFolder.loadLibraryFromFolder('assets', Sys.getCwd() + '${pathBack}assets/', true));
 		#elseif USE_ADAPTED_ASSETS
-			Paths.assetsTree.__defaultLibraries.push(ModsFolder.loadLibraryFromFolder('assets', './assets/', true));
+			Paths.assetsTree.__defaultLibraries.push(ModsFolder.loadLibraryFromFolder('assets', #if mobile SUtil.getStorageDirectory() + #else Sys.getCwd() + #end 'assets/', true));
 		#end
 
 
@@ -192,6 +207,9 @@ class Main extends Sprite
 		#end
 
 		initTransition();
+		#if mobile
+		LimeSystem.allowScreenTimeout = Options.screenTimeOut;
+		#end
 	}
 
 	public static function refreshAssets() {
